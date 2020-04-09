67b5702f388ec804db899c9e969c7e938051b511
Nigel Carty <nacarty@gmail.com>
2020-04-09 09:41:24 -0400

    Upgraded my controller

--- src/main/java/com/carty/controller/NigelController.java
+++ src/main/java/com/carty/controller/NigelController.java
@@ -1,12 +1,21 @@
 package com.carty.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class NigelController {
+	
 	@GetMapping("/get")
 	public String getMessage() {
 		return "This is your message from Nigel Controller";
 	}
+	
+	@GetMapping("/getaws")
+	public String getAWS() {
+		return "This is your AWS message from Nigel Controller";
+	}
+	
 }
